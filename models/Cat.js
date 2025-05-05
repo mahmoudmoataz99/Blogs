680da0315154df863658df9e053eddd543593141
mahmoudmoataz99 <mahmoudmoataz99@gmail.com>
2025-05-05 14:20:36 +0300

    Delete models directory

--- models/Cat.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const CategorySchema = new mongoose.Schema({
-  Name: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Category', CategorySchema);
